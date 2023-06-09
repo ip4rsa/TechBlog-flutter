95afca16019cbede2e33dccdf637908f7bf16a3e
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-06-09 20:50:25 +0330

    Add files via upload

---
+++ lib/_mySrtings.dart
@@ -0,0 +1,15 @@
+class myStrings {
+  static const String viewHotBlog = "داغ ترین مغاله ها";
+  static const String viewHotPodcast = "داغ ترین پادکست ها";
+  static const String relatedArticle = "نوشته های مرتبط";
+  static const String shareBlog = "ارسال مغاله";
+  static const String sharePodcast = "ارسال پادکست";
+  static const String editProfilePhoto = "ویرایش عکس پروفایل";
+  static const String favArticle = "مقالات مورد علاقه من";
+  static const String favePodcast = "پادکست های مورد علاقه من";
+  static const String logOute = "خروج از حساب کاربری";
+  static const String goToLoginText = """به تک‌بلاگ خوش اومدی
+
+برای ارسال مطلب و پادکست باید حتما 
+ثبت نام کنی """;
+}
