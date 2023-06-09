2ad47103675e78af7fae3375362a109af8385291
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-06-09 20:48:17 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,75 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:techblog/_myColors.dart';
+import 'package:techblog/view/_Splashscrn.dart';
+// import 'package:techblog/view/_Splashscrn.dart';
+// import 'package:techblog/view/goToLogin.dart';
+// import 'package:techblog/gen/assets.gen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: solidcolors.statusBarcolor,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: solidcolors.statusBarcolor));
   runApp(const MyApp());
-} 
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowMaterialGrid: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa', ''), // persian
+      ],
       theme: ThemeData(
- 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: 'dana',
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontFamily: 'danaBold',
+                fontSize: 17,
+                fontWeight: FontWeight.w700,
+                color: solidcolors.posterTitle),
+            subtitle1: TextStyle(
+                fontFamily: 'danaLight',
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+                color: solidcolors.posterSubtitle),
+            headline2: TextStyle(
+                fontFamily: 'danaLight',
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+                color: (Color.fromARGB(255, 255, 255, 255))),
+            subtitle2: TextStyle(
+                fontFamily: 'danaLight',
+                fontSize: 15.5,
+                fontWeight: FontWeight.w600,
+                color: Color.fromARGB(255, 11, 11, 11)),
+            headline3: TextStyle(
+                fontFamily: 'danaLight',
+                fontSize: 15,
+                fontWeight: FontWeight.w900,
+                color: Color.fromARGB(255, 40, 107, 174)),
+            headline4: TextStyle(
+                fontFamily: 'danaBold',
+                fontSize: 15,
+                fontWeight: FontWeight.w100,
+                color: Color.fromARGB(255, 15, 15, 15))),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SplashScreen(),
       ),
     );
   }
 }
+
