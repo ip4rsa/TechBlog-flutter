5f53ded5714abed9480f5a82464934b5f0d56cf6
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-06-09 20:52:07 +0330

    add my colors project

---
+++ lib/_myColors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class solidcolors {
+  static const Color primeryColor = Color.fromARGB(255, 64, 4, 87);
+  static const Color posterTitle = Color.fromARGB(255, 248, 248, 248);
+  static const Color posterSubtitle = Color.fromARGB(200, 248, 248, 248);
+  static const Color colorTitleBlue = Color.fromARGB(255, 40, 107, 174);
+  static const Color textTitle = Color.fromARGB(255, 10, 10, 10);
+  static const Color scafoldBg = Color.fromARGB(255, 254, 254, 254);
+  static const Color lightText = Color.fromARGB(255, 245, 245, 245);
+  static const Color selectedPodcast = Color.fromARGB(255, 245, 139, 26);
+  static const Color submitArticle = Color.fromARGB(255, 200, 200, 200);
+  static const Color submitPodcast = Color.fromARGB(255, 239, 239, 239);
+  static const Color subText = Color.fromARGB(255, 193, 193, 193);
+  static const Color statusBarcolor = Color.fromARGB(255, 250, 250, 250);
+  static const Color dividerCoror = Color.fromARGB(255, 112, 112, 112);
+}
+
+class gradiantColors {
+  static const List<Color> bottmNav = [
+    const Color.fromARGB(255, 25, 0, 94),
+    const Color.fromARGB(255, 68, 4, 88),
+  ];
+  static const List<Color> bottmNavBackgrond = [
+    Color.fromARGB(0, 255, 255, 255),
+    Color.fromARGB(255, 250, 250, 250),
+  ];
+  static const List<Color> hashtagListGradiant = [
+    Color.fromARGB(255, 0, 0, 0),
+    Color.fromARGB(255, 63, 63, 63),
+  ];
+  static const List<Color> posterCoverGradiant = [
+    const Color.fromARGB(0, 0, 0, 0),
+    const Color.fromARGB(170, 72, 20, 88),
+    const Color.fromARGB(255, 280, 20, 81),
+  ];
+
+  static const List<Color> articleCoverGradiant = [
+    Color.fromARGB(210, 0, 0, 0),
+    Color.fromARGB(0, 0, 0, 0),
+  ];
+}
