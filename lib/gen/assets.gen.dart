1dfcab73b4cc4c45caf65f8402320739fd70d8b1
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-06-10 17:30:55 +0330

    gen class

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,193 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsImgGen {
+  const $AssetsImgGen();
+
+  /// File path: assets/img/Hashtag.png
+  AssetGenImage get hashtag => const AssetGenImage('assets/img/Hashtag.png');
+
+  /// File path: assets/img/homeIcon.png
+  AssetGenImage get homeIcon => const AssetGenImage('assets/img/homeIcon.png');
+
+  /// File path: assets/img/logo.png
+  AssetGenImage get logo => const AssetGenImage('assets/img/logo.png');
+
+  /// File path: assets/img/posterImg.jpg
+  AssetGenImage get posterImg =>
+      const AssetGenImage('assets/img/posterImg.jpg');
+
+  /// File path: assets/img/sliderPhoto1.jpg
+  AssetGenImage get sliderPhoto1 =>
+      const AssetGenImage('assets/img/sliderPhoto1.jpg');
+
+  /// File path: assets/img/techBot.svg
+  SvgGenImage get techBot => const SvgGenImage('assets/img/techBot.svg');
+
+  /// File path: assets/img/userIcon.png
+  AssetGenImage get userIcon => const AssetGenImage('assets/img/userIcon.png');
+
+  /// File path: assets/img/userProfile.png
+  AssetGenImage get userProfile =>
+      const AssetGenImage('assets/img/userProfile.png');
+
+  /// File path: assets/img/writerIcon.png
+  AssetGenImage get writerIcon =>
+      const AssetGenImage('assets/img/writerIcon.png');
+
+  /// List of all assets
+  List<dynamic> get values => [
+        hashtag,
+        homeIcon,
+        logo,
+        posterImg,
+        sliderPhoto1,
+        techBot,
+        userIcon,
+        userProfile,
+        writerIcon
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsImgGen img = $AssetsImgGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
