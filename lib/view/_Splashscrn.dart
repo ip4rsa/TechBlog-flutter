20646e3261ff96246b870c06b7b55d65309d1067
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-06-13 13:05:07 +0330

    SplashScreen add

---
+++ lib/view/_Splashscrn.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:techblog/_myColors.dart';
+import 'package:techblog/view/mainScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 4)).then((value) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => homePage()));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SafeArea(
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/img/logo.png', height: 71),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 85,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    child: SpinKitThreeBounce(
+                        color: solidcolors.primeryColor, size: 19.0),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
