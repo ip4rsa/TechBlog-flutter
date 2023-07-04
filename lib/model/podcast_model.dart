4f102997f6b009dc883394f15b593349449a9c99
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-07-04 17:50:21 +0330

    add podcast Model

---
+++ lib/model/podcast_model.dart
@@ -0,0 +1,34 @@
+import 'package:techblog/component/_apiConstans.dart';
+
+class PodcastModel {
+  String? id;
+  String? title;
+  String? poster;
+  String? catName;
+  String? author;
+  String? view;
+  String? status;
+  String? createdAt;
+
+  PodcastModel({
+    required this.id,
+    required this.title,
+    required this.poster,
+    required this.catName,
+    required this.author,
+    required this.view,
+    required this.status,
+    required this.createdAt,
+  });
+
+  PodcastModel.fromJson(Map<String, dynamic> element) {
+    id = element["id"];
+    title = element["title"];
+    poster = apiConstant.hostDLurl + element["poster"];
+    catName = element["cat_name"];
+    author = element["author"];
+    view = element["view"];
+    status = element["status"];
+    createdAt = element["created_at"];
+  }
+}
