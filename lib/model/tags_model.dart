e0f12fc401edd0998bec98fa3048891e2f90b42c
Parsa Sajjadian <117844346+ip4rsa@users.noreply.github.com>
2023-07-04 23:36:07 +0330

    tags Model

---
+++ lib/model/tags_model.dart
@@ -0,0 +1,14 @@
+class TagsModel {
+  String? id;
+  String? title;
+
+  TagsModel({
+    required this.id,
+    required this.title,
+  });
+
+  TagsModel.fromJason(Map<String, dynamic> element) {
+    id = element["id"];
+    title = element["title"];
+  }
+}
